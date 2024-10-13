f775c1ecf2cd80550f5fa1fff45d1e82b958f172
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-10-12 20:56:07 -0400

    Chapter 14 Models and data, 2.3. Add Model Identifier

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,6 +1,11 @@
 package org.launchcode.codingevents.models;
 
+import java.util.Objects;
+
 public class Event {
+
+    private int id;
+    private static int nextID = 1;
 
     private String name;
     private String description;
@@ -8,6 +13,8 @@ public class Event {
     public Event(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextID;
+        nextID++;
     }
 
     public String getName() {
@@ -22,13 +29,29 @@ public class Event {
         this.description = description;
     }
 
+    public int getId() {
+        return id;
+    }
+
     public void setName(String name) {
         this.name = name;
-
     }
 
     @Override
     public String toString() {
         return name;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Event event)) return false;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }

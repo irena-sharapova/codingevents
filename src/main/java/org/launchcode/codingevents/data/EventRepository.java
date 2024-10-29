16959cd44ba5fd627dd0daa6df67a38232c838cc
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-10-28 21:50:28 -0400

    Chapter 17. ORM chapter example fixed

--- src/main/java/org/launchcode/codingevents/data/EventRepository.java
+++ src/main/java/org/launchcode/codingevents/data/EventRepository.java
@@ -7,7 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository  extends CrudRepository<Event, Integer>{
-
 }
-
 // public class MyRepository implements EventRepository {}

7e9ccfde5f87d7a9146037745d1c5cf64b70d8bf
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-11-06 22:19:25 -0500

    Chapter 18. Many-to-many

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -7,6 +7,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -28,6 +30,9 @@ public class Event extends AbstractEntity {
     @ManyToOne
     @NotNull(message = "Category is required")
     private EventCategory eventCategory;
+
+    @ManyToMany
+    private final List<Tag> tags = new ArrayList<>();
 
     public Event(String name, EventCategory eventCategory) {
         this.name = name;
@@ -58,6 +63,14 @@ public class Event extends AbstractEntity {
 
     public void setEventDetails(EventDetails eventDetails) {
         this.eventDetails = eventDetails;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void addTag(Tag tag) {
+        this.tags.add(tag);
     }
 
     @Override
--- src/main/java/org/launchcode/codingevents/models/Tag.java
+++ src/main/java/org/launchcode/codingevents/models/Tag.java
@@ -1,19 +1,29 @@
 package org.launchcode.codingevents.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-    @Entity
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
     public class Tag extends AbstractEntity {
 
         @Size(min = 1, max = 25)
         @NotBlank
         private String name;
 
+        @ManyToMany(mappedBy = "tags")
+        private final List<Event> events = new ArrayList<>();
+
+
         public Tag(String name) {
             this.name = name;
         }
+
 
         public Tag() {}
 
@@ -29,6 +39,9 @@ import jakarta.validation.constraints.Size;
             this.name = name;
         }
 
+    public List<Event> getEvents() {
+        return events;
     }
+}
 
 
